84f3983d0988041ae4e67a204e58ef6c68b742f4
Mackensie Jack <kensiejack1@gmail.com>
2023-11-03 17:13:09 -0400

    release branch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1>Hello from release branch!</h1>
+        <a href="https://main.d346ryumzun9ji.amplifyapp.com/">go to main</a>
+        <a href="https://develop.d346ryumzun9ji.amplifyapp.com/">
+          go to develop
         </a>
       </header>
     </div>
