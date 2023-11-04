230336b1416f4402e1c5909ee3ef1a388210ea97
Mackensie Jack <kensiejack1@gmail.com>
2023-11-04 07:28:02 -0400

    main links

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1>Hello from main branch!</h1>
+        <a href="https://main.d346ryumzun9ji.amplifyapp.com/">go to main</a>
+        <a href="https://develop.d346ryumzun9ji.amplifyapp.com/">
+          go to develop
+        </a>
+        <a href="https://release.d346ryumzun9ji.amplifyapp.com/">
+          go to release
         </a>
       </header>
     </div>
