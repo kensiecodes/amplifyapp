9eacf64c5e046ed7a6bec2604b697b142decea83
Mackensie Jack <kensiejack1@gmail.com>
2023-11-04 07:17:09 -0400

    feature branch

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from release branch!</h1>
+        <h1>Hello from feature branch!</h1>
         <a href="https://main.d346ryumzun9ji.amplifyapp.com/">go to main</a>
         <a href="https://develop.d346ryumzun9ji.amplifyapp.com/">
           go to develop
+        </a>
+        <a href="https://release.d346ryumzun9ji.amplifyapp.com/">
+          go to release
         </a>
       </header>
     </div>
